8d3305c40cc4fb148ead127885c1b3ceeb97bf05
Wouter de Vos <wouter@codaisseur.com>
2018-09-07 16:23:54 +0200

    Use the db in a request

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
-const app = express()
+const database = require('./models')
 
+const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  const userCount = database.User.findAll()
+    .then((results) => {
+      res.send(`We have ${results.length} users`)
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
